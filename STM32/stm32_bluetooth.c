142345ff23d73b0b54a9c43e3eb671478e605423
qlqlrh <dongin@pusan.ac.kr>
2024-12-21 17:21:31 +0900

    PLAY 명령어 폰으로 전송

---
+++ STM32/stm32_bluetooth.c
@@ -0,0 +1,157 @@
+// PLAY 명령어 블루투스 모듈에게 보내는 코드 (정상 동작 확인)
+
+#include "misc.h"
+
+/* function prototype */
+void RCC_Configure(void);
+void GPIO_Configure(void);
+void USART1_Init(void);
+void USART2_Init(void);
+void NVIC_Configure(void);
+
+void RCC_Configure(void)
+{  
+    /* USART1, USART2 TX/RX port clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);    
+    /* USART1, USART2 clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);  
+    /* Alternate Function IO clock enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+}
+
+void GPIO_Configure(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    
+    /* USART1 pin setting */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);    
+    //RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);    
+    /* USART2 pin setting */
+    //TX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);    
+    //RX
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+}
+
+void USART1_Init(void)
+{
+    USART_InitTypeDef USART1_InitStructure;
+
+    USART_Cmd(USART1, ENABLE);
+
+    USART1_InitStructure.USART_BaudRate = 9600;
+    USART1_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART1_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART1_InitStructure.USART_Parity = USART_Parity_No;
+    USART1_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART1_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_Init(USART1, &USART1_InitStructure);
+
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
+
+void USART2_Init(void)
+{
+    USART_InitTypeDef USART2_InitStructure;
+
+    USART_Cmd(USART2, ENABLE);
+
+    USART2_InitStructure.USART_BaudRate = 9600;
+    USART2_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART2_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART2_InitStructure.USART_Parity = USART_Parity_No;
+    USART2_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART2_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_Init(USART2, &USART2_InitStructure);    
+
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);    
+}
+
+void NVIC_Configure(void) {
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+
+    // USART1
+    NVIC_EnableIRQ(USART1_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    // USART2
+    NVIC_EnableIRQ(USART2_IRQn);
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
+
+void USART1_IRQHandler() {
+    uint16_t word;
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET){
+        word = USART_ReceiveData(USART1);
+        USART_SendData(USART2, word);
+        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+    }
+}
+
+void USART2_IRQHandler() {
+    uint16_t word;
+    if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET){
+        word = USART_ReceiveData(USART2);
+        USART_SendData(USART1, word);
+        USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+    }
+}
+
+void delay_ms(uint32_t ms) {
+    for (uint32_t i = 0; i < ms * 4000; i++) {
+        __NOP(); // Simple delay loop
+    }
+}
+
+int main(void)
+{
+    char msg[] = "PLAY\r\n";
+
+    SystemInit();
+
+    RCC_Configure();
+
+    GPIO_Configure();
+
+    USART1_Init();      // pc
+
+    USART2_Init();      // bluetooth
+
+    NVIC_Configure();
+
+    while (1) {
+        USART_SendData(USART2, msg[0]); // Send "PLAY\r\n" via USART2
+        for (int i = 1; i < sizeof(msg) - 1; i++) {
+            while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
+            USART_SendData(USART2, msg[i]);
+        }
+
+        delay_ms(10000); // Wait for 10 seconds
+    }
+
+    return 0;
+}
